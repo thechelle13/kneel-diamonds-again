90282145ace401a6b7f9d5d976e169fbbe8a76d8
Michelle Totherow <tym4me13@gmail.com>
2023-11-01 11:32:54 -0500

    update to OrderView for working expand

--- src/orders_view.py
+++ src/orders_view.py
@@ -5,32 +5,27 @@ from repository import db_get_all, db_get_single,db_create,db_delete,db_update
 class OrderView():
     
     
-    def get_expanded(self, handler, pk):
+    def get_expanded(self, handler, pk, expand_params):
         if pk != 0:
+            # Original SQL query for fetching an order
             sql = """
-            SELECT o.id, o.metalId, o.sizeId, o.sytleId AS order_data
+            SELECT o.id, o.metalId, o.sizeId, o.styleId
             FROM orders o
-            LEFT JOIN orders o ON o.ordersId = o.id
             WHERE o.id = ?
             """
             query_results = db_get_single(sql, pk)
 
             if query_results:
                 order_data = dict(query_results)
-                response = {
-                    "id": order_data["id"],
-                    "metal_id": order_data["metal_id"],
-                    "size_id": order_data["size_id"],
-                    "style_id": order_data["style_id"]
-                }
-                return handler.response(json.dumps(response), status.HTTP_200_SUCCESS.value)
+                expanded_response = self.expand_related_resources(order_data, expand_params)
+                return handler.response(json.dumps(expanded_response), status.HTTP_200_SUCCESS.value)
             else:
                 return "Error", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
         else:
+            # Original SQL query for fetching all orders
             sql = """
-            SELECT o.id, o.metalId, o.sizeId, o.styleId AS order_data
-            FROM Orders o
-            LEFT JOIN Orders o ON o.id = o.id
+            SELECT o.id, o.metalId, o.sizeId, o.styleId
+            FROM orders o
             """
             query_results = db_get_all(sql)
 
@@ -38,18 +33,56 @@ class OrderView():
 
             for row in query_results:
                 order_data = dict(row)
-                response = {
-                    "id": order_data["id"],
-                    "metal_id": order_data["metal_id"],
-                    "size_id": order_data["size_id"],
-                    "style_id": order_data["style_id"]
-                }
-                expanded_orders.append(response)
+                expanded_response = self.expand_related_resources(order_data, expand_params)
+                expanded_orders.append(expanded_response)
 
             if expanded_orders:
                 return handler.response(json.dumps(expanded_orders), status.HTTP_200_SUCCESS.value)
             else:
                 return handler.response("Error", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+
+    def expand_related_resources(self, order_data, expand_params):
+        expanded_response = {
+            "id": order_data["id"],
+            "metalId": order_data["metalId"],
+            "sizeId": order_data["sizeId"],
+            "styleId": order_data["styleId"]
+        }
+
+        for param in expand_params:
+            if param == 'metal':
+                # Fetch and add the 'metal' resource
+                metal_data = self.get_expanded_metal(order_data["metalId"])
+                expanded_response["metal"] = metal_data
+            elif param == 'size':
+                # Fetch and add the 'size' resource
+                size_data = self.get_expanded_size(order_data["sizeId"])
+                expanded_response["size"] = size_data
+            elif param == 'style':
+                # Fetch and add the 'style' resource
+                style_data = self.get_expanded_style(order_data["styleId"])
+                expanded_response["style"] = style_data
+
+        return expanded_response
+
+    def get_expanded_metal(self, metal_id):
+        # Original SQL query to fetch metal data
+        sql = "SELECT id, metal, price FROM metals WHERE id = ?"
+        query_results = db_get_single(sql, metal_id)
+        return dict(query_results)
+
+    def get_expanded_size(self, size_id):
+        # Original SQL query to fetch size data
+        sql = "SELECT id, carets, price FROM sizes WHERE id = ?"
+        query_results = db_get_single(sql, size_id)
+        return dict(query_results)
+
+    def get_expanded_style(self, style_id):
+        # Original SQL query to fetch style data
+        sql = "SELECT id, style, price FROM styles WHERE id = ?"
+        query_results = db_get_single(sql, style_id)
+        return dict(query_results)
+
     
     
     
